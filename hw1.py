6c7d08260569f76b13ec8ff7141bd22d9ce7db62
EnWa1 <80194765+enwa1ker@users.noreply.github.com>
2026-04-03 16:54:23 +0600

    Implement click counter using Flet framework
    Refactor the button click handler and integrate Flet for UI.

--- hw1.py
+++ hw1.py
@@ -1,12 +1,19 @@
-count = 0
+import flet as ft
 
-def setup_button():
-    def on_button_click():
-        global count 
+def main(page: ft.Page):
+    page.title = "Счётчик нажатий"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    count = 0
+    text_hello = ft.Text("Нажато: 0 раз", size=30)
+
+    def on_button_click(e):
+        nonlocal count
         count += 1
-        print(f"Нажато {count} раз")
-    return on_button_click
+        text_hello.value = f"Нажато: {count} 
+        page.update()
+    btn = ft.ElevatedButton("Нажми на меня", on_click=on_button_click)
+    page.add(text_hello, btn)
 
-click_handler = setup_button()
-click_handler() 
-click_handler() 
+ft.app(target=main)
